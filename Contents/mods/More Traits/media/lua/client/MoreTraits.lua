95ad934c72dc4958d02d6569d1788be9b72fd1cc
Fajdek <100788476+Fajdek@users.noreply.github.com>
2022-04-03 16:25:15 +0200

    functionality for trait
    not finished yet!!! pls ignore if it appears in discord XD

--- Contents/mods/More Traits/media/lua/client/MoreTraits.lua
+++ Contents/mods/More Traits/media/lua/client/MoreTraits.lua
@@ -253,6 +253,17 @@ local function initToadTraitsItems(_player)
             end
         end
     end
+    
+    if player:HasTrait("preparedcoordination") then
+	inv:AddItem("MuldraughMap");
+	inv:AddItem("RosewoodMap");
+	inv:AddItem("RiversideMap");
+	inv:AddItem("WestpointMap");
+	inv:AddItem("MarchRidgeMap");
+	inv:AddItem("Pencil");
+	inv:AddItem("DigitalWatch2");
+	end
+    
     if player:HasTrait("drinker") then
         if SandboxVars.MoreTraits.AlcoholicFreeDrink == true then
             inv:AddItem("Base.WhiskeyFull");
