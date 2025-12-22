16c46d5fe490804d10cc84eac1871ec90810df4d
FallenxSowerby <alexander.sowerby@live.co.uk>
2025-12-22 14:49:27 +0000

    B42.13 Forage fixes

--- Contents/mods/More Traits/42.13/media/lua/shared/Foraging/ToadTraitsforageDefinitions.lua
+++ Contents/mods/More Traits/42.13/media/lua/shared/Foraging/ToadTraitsforageDefinitions.lua
@@ -1,135 +1,149 @@
---[[
 require "Foraging/forageDefinitions";
-MoreTraitsSkills = {
-	incomprehensive = {
-		name = "incomprehensive",
-		type = "trait",
-		visionBonus = -1.0,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["ForestRarities"] = -5,
-			["Medical"] = -5,
-			["Ammunition"] = -5,
-			["JunkWeapons"] = -5,
-		},
-	},
-	scrounger = {
-		name = "scrounger",
-		type = "trait",
-		visionBonus = 1.0,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["ForestRarities"] = 5,
-			["Medical"] = 5,
-			["Ammunition"] = 5,
-			["JunkWeapons"] = 5,
-		},
-	},
-	vagabond = {
-		name = "vagabond",
-		type = "trait",
-		visionBonus = 0.7,
-		weatherEffect = 13,
-		darknessEffect = 3,
-		specialisations = {
-			["MedicinalPlants"] = 5,
-			["Trash"] = 10,
-		},
-	},
-	wildsman = {
-		name = "wildsman",
-		type = "trait",
-		visionBonus = 0.5,
-		weatherEffect = 13,
-		darknessEffect = 5,
-		specialisations = {
-			["Animals"] = 5,
-			["Berries"] = 3,
-			["Mushrooms"] = 3,
-			["MedicinalPlants"] = 3,
-		},
-	},
-	specfood = {
-		name = "specfood",
-		type = "trait",
-		visionBonus = 0.4,
-		weatherEffect = 13,
-		darknessEffect = 5,
-		specialisations = {
-			["Animals"] = 5,
-			["Berries"] = 5,
-			["Mushrooms"] = 5,
-			["MedicinalPlants"] = 5,
-			["WildPlants"] = 5,
-		},
-	},
-	natural = {
-		name = "natural",
-		type = "trait",
-		visionBonus = 0.2,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["Animals"] = 5,
-			["Berries"] = 5,
-			["Mushrooms"] = 5,
-			["JunkFood"] = 5,
-			["MedicinalPlants"] = 3,
-			["WildPlants"] = 5,
-		},
-	},
-	specaid = {
-		name = "specaid",
-		type = "trait",
-		visionBonus = 0.2,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["MedicinalPlants"] = 15,
-			["WildPlants"] = 5,
-			["Crops"] = 5,
-			["Berries"] = 5,
-			["Mushrooms"] = 5,
-		},
-	},
-	gourmand = {
-		name = "gourmand",
-		type = "trait",
-		visionBonus = 0,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["Animals"] = 3,
-			["Berries"] = 3,
-			["Mushrooms"] = 3,
-			["JunkFood"] = 3,
-		},
-	},
-	gunspecialist = {
-		name = "gunspecialist",
-		type = "trait",
-		visionBonus = 0.5,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["Ammunition"] = 3,
-		},
-	},
-	ingenuitive = {
-		name = "ingenuitive",
-		type = "trait",
-		visionBonus = 0,
-		weatherEffect = 0,
-		darknessEffect = 0,
-		specialisations = {
-			["Insects"] = 5,
-			["FishBait"] = 5,
-		},
-	},
+
+local ToadTraits = {
+    incomprehensive = {
+        name = "ToadTraits:incomprehensive",
+        type = "trait",
+        visionBonus = -1.0,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["ForestRarities"] = -5,
+            ["Medical"] = -5,
+            ["Ammunition"] = -5,
+            ["JunkWeapons"] = -5,
+        },
+        testFuncs = {},
+    },
+    scrounger = {
+        name = "ToadTraits:scrounger",
+        type = "trait",
+        visionBonus = 1.0,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["ForestRarities"] = 5,
+            ["Medical"] = 5,
+            ["Ammunition"] = 5,
+            ["JunkWeapons"] = 5,
+        },
+        testFuncs = {},
+    },
+    vagabond = {
+        name = "ToadTraits:vagabond",
+        type = "trait",
+        visionBonus = 0.7,
+        weatherEffect = 13,
+        darknessEffect = 3,
+        specialisations = {
+            ["MedicinalPlants"] = 5,
+            ["Trash"] = 10,
+        },
+        testFuncs = {},
+    },
+    wildsman = {
+        name = "ToadTraits:wildsman",
+        type = "trait",
+        visionBonus = 0.5,
+        weatherEffect = 13,
+        darknessEffect = 5,
+        specialisations = {
+            ["Animals"] = 5,
+            ["Berries"] = 3,
+            ["Mushrooms"] = 3,
+            ["MedicinalPlants"] = 3,
+        },
+        testFuncs = {},
+    },
+    specfood = {
+        name = "ToadTraits:specfood",
+        type = "trait",
+        visionBonus = 0.4,
+        weatherEffect = 13,
+        darknessEffect = 5,
+        specialisations = {
+            ["Animals"] = 5,
+            ["Berries"] = 5,
+            ["Mushrooms"] = 5,
+            ["MedicinalPlants"] = 5,
+            ["WildPlants"] = 5,
+        },
+        testFuncs = {},
+    },
+    natural = {
+        name = "ToadTraits:natural",
+        type = "trait",
+        visionBonus = 0.2,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["Animals"] = 5,
+            ["Berries"] = 5,
+            ["Mushrooms"] = 5,
+            ["JunkFood"] = 5,
+            ["MedicinalPlants"] = 3,
+            ["WildPlants"] = 5,
+        },
+        testFuncs = {},
+    },
+    specaid = {
+        name = "ToadTraits:specaid",
+        type = "trait",
+        visionBonus = 0.2,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["MedicinalPlants"] = 15,
+            ["WildPlants"] = 5,
+            ["Crops"] = 5,
+            ["Berries"] = 5,
+            ["Mushrooms"] = 5,
+        },
+        testFuncs = {},
+    },
+    gourmand = {
+        name = "ToadTraits:gourmand",
+        type = "trait",
+        visionBonus = 0,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["Animals"] = 3,
+            ["Berries"] = 3,
+            ["Mushrooms"] = 25,
+            ["JunkFood"] = 3,
+        },
+        testFuncs = {},
+    },
+    gunspecialist = {
+        name = "ToadTraits:gunspecialist",
+        type = "trait",
+        visionBonus = 0.5,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["Ammunition"] = 3,
+        },
+        testFuncs = {},
+    },
+    ingenuitive = {
+        name = "ToadTraits:ingenuitive",
+        type = "trait",
+        visionBonus = 0,
+        weatherEffect = 0,
+        darknessEffect = 0,
+        specialisations = {
+            ["Insects"] = 5,
+            ["FishBait"] = 5,
+        },
+        testFuncs = {},
+    },
 };
-for skillName, skillDef in pairs(MoreTraitsSkills) do
-	table.insert(forageSystem.forageSkillDefinitions, skillDef);
+
+local function onPreAddSkillDefs(forageSystem)
+    for _, def in pairs(ToadTraits) do
+        forageSystem.addSkillDef(def);
+    end
 end
---]]
+
+Events.preAddSkillDefs.Add(onPreAddSkillDefs);
